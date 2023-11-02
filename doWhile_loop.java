401a8fb8014eb18a90db51937effee1d97d713bc
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 23:13:40 +0530

    doWhile_loop.java

---
+++ doWhile_loop.java
@@ -0,0 +1,29 @@
+public class doWhile_loop
+{
+	public static void main(String[] args) {
+	    
+	    
+	    /*do -while loop syntax 
+	    
+	    //initialization
+	    do
+	    {
+	        //body of do
+	        //update step
+	    }(condition);
+	    */
+	    int i=0;
+	    do
+	    {
+	        System.out.println(i);
+	        i++;
+	    }while(i<=5);
+		
+		
+		
+		
+		
+		
+	}
+}
+
