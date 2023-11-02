7696e733c05e746c44de189ca4e9f14b1211c37a
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 23:22:52 +0530

    If-else_statement.java

---
+++ If-else_statement.java
@@ -0,0 +1,35 @@
+public class If-else_statement
+{
+	public static void main(String[] args) {
+	    
+	    
+	 
+	   /*  if-else syntax
+	    if(condition)
+	    {
+	        //body of if 
+	    }
+	    else
+	    {
+	        //body of else
+	    }
+	    
+	    */
+     int num=10;
+	   if(num%2==0)
+	   {
+	        System.out.println(num +" is even number");  
+	   }
+	   else
+	   {
+	       System.out.println(num+" is odd number");
+	   }
+		
+		
+		
+		
+		
+		
+	}
+}
+
