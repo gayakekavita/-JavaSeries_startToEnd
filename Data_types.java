271e34c28d5f87c68782fa68fcbec8ee868cbcf8
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 22:27:39 +0530

    Data_types.java

---
+++ Data_types.java
@@ -0,0 +1,28 @@
+public class Data_types
+{
+	public static void main(String[] args) {
+	    //integer data Type
+		int num=20; 
+		System.out.println(num);
+		
+		//string data type
+		String str="kavya"; 
+		System.out.println(str);
+		
+		//float data type 
+		float percentage=90.80f;
+		System.out.println(percentage);
+		
+		
+		//double data type
+		double num1=20.30;
+		System.out.println(num1);
+		
+		
+		//boolean data type 
+		boolean  result=true;
+		System.out.println(result);
+		
+		
+	}
+}
