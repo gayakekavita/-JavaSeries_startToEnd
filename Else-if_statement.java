2fec219f18f9c28c531e1b37cc2c45078615c211
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 23:30:53 +0530

    Else-if_statement.java

---
+++ Else-if_statement.java
@@ -0,0 +1,43 @@
+public class Else-if_statement
+{
+	public static void main(String[] args) {
+	    
+	    
+	   int num=5;
+	   /*  else-if  syntax
+	    if(condition)
+	    {
+	        //body of if 
+	    }
+	    else if(condition)
+	    {
+	        //body of else-if 
+	    }
+	    else
+	    {
+	        //body of else 
+	    }
+	    
+	    */
+	   if(num>0)
+	   {
+	        System.out.println(num +" positive number");
+	        
+	   }
+	   else if(num<0)
+	   {
+	       System.out.println(num+" is negative number");
+	   }
+	   else 
+	   {
+	       System.out.println(num +" is  zero");   
+	   }
+	   
+		
+		
+		
+		
+		
+		
+	}
+}
