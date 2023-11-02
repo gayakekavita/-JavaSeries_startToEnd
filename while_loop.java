49d5eeeb89c57bbffc1cd7c9f84cac20622fe8fb
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 22:58:08 +0530

    while_loop.java

---
+++ while_loop.java
@@ -0,0 +1,28 @@
+public class while_loop
+{
+	public static void main(String[] args) {
+	    
+	    
+	    /* while loop syntax 
+	    
+	    //initialization
+	    while(condition)
+	    {
+	        //body of for loop
+	        //update step
+	    }
+	    */
+	    int i=0;
+	    while(i<=5)
+	    {
+	        System.out.println(i);
+	        i++;
+	    }
+		
+		
+		
+		
+		
+		
+	}
+}
