06c1209a3a12820dbb08f4ce30c7af0e8d4fb1a6
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 22:53:04 +0530

    for_loop.java

---
+++ for_loop.java
@@ -0,0 +1,22 @@
+public class for_loop
+{
+	public static void main(String[] args) {
+	    //for loop
+      //simple program 
+     /* for( variable intialization;condition;update step;)
+	    {
+	        //body of for loop
+	    }
+	    */
+	    for(int i=0;i<=5;i++)
+	    {
+	        System.out.println(i);
+	    }
+		
+		
+		
+		
+		
+		
+	}
+}
