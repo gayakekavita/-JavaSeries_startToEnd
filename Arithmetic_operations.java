d45bcaa39a3fdd352d9cdfd6032097b75c108c47
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-02 22:39:05 +0530

    Arithmetic_operations.java

---
+++ Arithmetic_operations.java
@@ -0,0 +1,25 @@
+public class Arithmetic_operations
+{
+	public static void main(String[] args) {
+	    //Arithmetic Operations 
+		int num1=20; 
+		int num2=10;
+		
+		int addition=num1+num2;
+		int subtraction=num1-num2;
+		int multiplication=num1*num2;
+		float division=num1/num2;
+		int remainder=num1%num2;
+		System.out.println("addition:"+addition);
+		System.out.println("subtraction:"+subtraction);
+		System.out.println("multiplication:"+multiplication);
+		System.out.println("division:"+division);
+		System.out.println("remainder:"+remainder);
+		
+		
+		
+		
+		
+		
+	}
+}
