df3e2f48a840792c78c7da0fc538f052ecd21dca
gayakekavita <130667728+gayakekavita@users.noreply.github.com>
2023-11-09 22:11:16 +0530

    pattern_0.java

--- pattern_0.java
+++ pattern_0.java
@@ -1,1 +1,37 @@
+/* 000000
+   000000
+   000000
+   000000
+   000000
 
+    Q::print above pattern */
+
+public class pattern_0
+{
+	public static void main(String[] args)
+  {
+		for(int i=0;i<5;i++)
+		{
+		    for(int j=0;j<6;j++)
+		    {
+		      System.out.print('0'+""); 
+		    }
+		    System.out.println();
+		    
+		}
+	}
+}
+
+/*    step1: find out how many rows are required for printing  given pattern
+      step: find out how many columns are required for i'th (each row) rows
+      step 3:implementation of program 
+      step 4: execution of program
+      step 5: output will look like
+              000000
+              000000
+              000000
+              000000
+              000000
+              
+
+  */
